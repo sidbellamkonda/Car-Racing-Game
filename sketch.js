81b5e5651b1d35ebb62e319ce852e75701d273bc
sidbellamkonda <71858026+sidbellamkonda@users.noreply.github.com>
2020-12-10 19:58:07 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas, database;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+
+
+}
+
+function draw(){
+
+}
